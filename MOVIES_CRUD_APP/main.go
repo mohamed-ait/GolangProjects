74faf94aae1f480deb2f7be5de665e69ef2dc9c7
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-09 15:46:44 +0100

    Updated MOVIES_CRUD_APP/main.go file

--- MOVIES_CRUD_APP/main.go
+++ MOVIES_CRUD_APP/main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	//"fmt"
+	//"log"
+	//"encoding/json"
+	//"math/rand"
+	//"net/http"
+	//"strconv"
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
-	ID    string `json:"id"`
-	Isbn  string `json:"isbn"`
-	Title string `json:"title"`
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
 }
