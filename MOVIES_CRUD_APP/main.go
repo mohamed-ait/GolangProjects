9126252352e82ab796ddce0877b5796cc1e22919
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-09 16:08:24 +0100

    Updated MOVIES_CRUD_APP/main.go

--- MOVIES_CRUD_APP/main.go
+++ MOVIES_CRUD_APP/main.go
@@ -28,5 +28,9 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 }
