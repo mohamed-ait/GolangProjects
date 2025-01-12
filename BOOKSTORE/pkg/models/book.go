7b75dee2e0b05b8b807bd268470dc19fc361c8fa
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-12 17:33:03 +0100

    Updated BOOKSTORE/pkg/models/book.go

---
+++ BOOKSTORE/pkg/models/book.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"GolangProjects/BOOKSTORE/pkg/config"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+}
+func main() {
+
+}
