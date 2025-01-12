45ba660796190128ee6cb3fa033f2aa2565acf9e
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-12 18:22:41 +0100

    Updated  BOOKSTORE/cmd/main/main.go

---
+++ BOOKSTORE/cmd/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"GolangProjects/BOOKSTORE/pkg/routes"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+}
