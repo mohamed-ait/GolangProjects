409323fdfa21e92021321d9157a2bf5fd33a4818
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-13 22:01:15 +0100

    Updated BOOKSTORE/pkg/controllers/book-controller.go

--- BOOKSTORE/pkg/controllers/book-controller.go
+++ BOOKSTORE/pkg/controllers/book-controller.go
@@ -41,6 +41,21 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	utils.ParseBody(r, newBook) //parse to database format
 	b := newBook.CreateBook()
 	res, _ := json.Marshal(b)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["booId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing !")
+	}
+	b := models.DeleteBook(ID)
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
