35a35db4ab45b7eb09258079cfd7cfdc8a71024d
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-14 00:02:15 +0100

    Updated  BOOKSTORE/cmd/main/main.go

--- BOOKSTORE/cmd/main/main.go
+++ BOOKSTORE/cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"GolangProjects/BOOKSTORE/pkg/routes"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,5 +13,6 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
+	fmt.Println("Starting server at port 9010")
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
