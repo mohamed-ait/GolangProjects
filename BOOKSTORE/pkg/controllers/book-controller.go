1ef69e3763f5a33c5b7f941298fad55904e0e641
Mohamed AIT LAHCEN <mohamedaitlahcen62@gamil.com>
2025-01-14 01:13:44 +0100

    Updated  BOOKSTORE/pkg/controllers/book-controller.go

--- BOOKSTORE/pkg/controllers/book-controller.go
+++ BOOKSTORE/pkg/controllers/book-controller.go
@@ -64,8 +64,8 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	updateBook := &models.Book{}
 	utils.ParseBody(r, updateBook)
 	vars := mux.Vars(r)
-	bookId := vars["bookId"]
-	ID, err := strconv.ParseInt(bookId, 0, 0)
+	BId := vars["bookId"]
+	ID, err := strconv.ParseInt(BId, 0, 0)
 	if err != nil {
 		fmt.Println("Error while parsing !")
 	}
